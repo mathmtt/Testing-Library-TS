be39e16501a9062dc0d9811d57c47de23222e44a
Matheus Motta <matheusmotta136@gmail.com>
2023-06-27 14:54:38 -0300

    requisito 01

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o link Home na aplicação', () => {
+  test('A aplicação contém um conjunto fixo de links de navegação.', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /home/i });
+    const about = screen.getByRole('link', { name: /about/i });
+    const favoritePokemon = screen.getByRole('link', {
+      name: /Favorite Pokémon/i,
+    });
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemon).toBeInTheDocument();
+  });
+});
