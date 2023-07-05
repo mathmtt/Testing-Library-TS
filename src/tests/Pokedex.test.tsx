c6648e4bdc4f567c6548f86516fb89e6cb1705b2
Matheus Motta <matheusmotta136@gmail.com>
2023-07-04 23:13:12 -0300

    requisito 06

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,73 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente Pokedex.tsx', () => {
+  test('Teste se a página contém um heading h2 com o texto Encountered Pokémon', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('heading', { name: /Encountered Pokémon/i })).toBeInTheDocument();
+  });
+
+  test('Os próximos Pokémon da lista devem ser mostrados, um a um, ao clicar sucessivamente no botão', () => {
+    const arrAllPokemons = [
+      { name: /pikachu/i },
+      { name: /charmander/i },
+      { name: /caterpie/i },
+      { name: /ekans/i },
+      { name: /alakazam/i },
+      { name: /mew/i },
+      { name: /rapidash/i },
+      { name: /snorlax/i },
+      { name: /dragonair/i },
+    ];
+    renderWithRouter(<App />);
+    const nextPokemonButton = screen.getByRole('button', { name: /Próximo Pokémon/i });
+    expect(nextPokemonButton).toBeInTheDocument();
+
+    arrAllPokemons.forEach((pokemon) => {
+      screen.getByText(pokemon.name); // Verifica se o nome do Pokémon está presente na tela antes de clicar no botão
+      screen.getByRole('button', { name: /Próximo Pokémon/i }).click();
+    });
+  });
+
+  test('Teste se é mostrado apenas um pokémon por vez', () => {
+    renderWithRouter(<App />);
+    expect(screen.getAllByTestId(/pokemon-name/i)).toHaveLength(1);
+  });
+
+  test('Após a seleção de um botão de tipo, a Pokédex deve circular somente pelos Pokémon daquele tipo', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('button', { name: /Próximo Pokémon/i })).toBeInTheDocument();
+
+    const buttons = {
+      all: screen.getByRole('button', { name: /all/i }),
+      electric: screen.getByRole('button', { name: /electric/i }),
+      fire: screen.getByRole('button', { name: /fire/i }),
+      bug: screen.getByRole('button', { name: /bug/i }),
+      poison: screen.getByRole('button', { name: /poison/i }),
+      psychic: screen.getByRole('button', { name: /psychic/i }),
+      normal: screen.getByRole('button', { name: /normal/i }),
+      dragon: screen.getByRole('button', { name: /dragon/i }),
+      nextPokemon: screen.getByRole('button', { name: /próximo pokémon/i }),
+    };
+
+    const typePokemonMap = {
+      electric: 'Pikachu',
+      fire: 'Charmander',
+      bug: 'Caterpie',
+      poison: 'Ekans',
+      psychic: 'Alakazam',
+      normal: 'Snorlax',
+      dragon: 'Dragonair',
+    };
+
+    Object.entries(typePokemonMap).forEach(([type, pokemonName]) => {
+      screen.getByRole('button', { name: type }).click();
+      expect(screen.getByText(pokemonName)).toBeInTheDocument();
+      expect(buttons.nextPokemon).toBeDisabled();
+    });
+
+    buttons.all.click();
+    expect(buttons.nextPokemon).toBeEnabled();
+  });
+});
--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,1 +1,36 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Se o arquivo-teste Pokemon.test.tsx contempla 100% dos casos de uso criados pelo Stryker:', async () => {
+  const { user } = renderWithRouter(<App />);
+  const pokemonImage = screen.getByRole('img', { name: /pikachu sprite/i });
+  expect(pokemonImage).toBeInTheDocument();
+  expect(pokemonImage)
+    .toHaveAttribute('src', 'https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png');
+  expect(pokemonImage).toHaveAttribute('alt', 'Pikachu sprite');
+
+  const type = screen.getByTestId('pokemon-type');
+  expect(type).toBeInTheDocument();
+  expect(type).toHaveTextContent(/electric/i);
+
+  const pokemonName = screen.getByTestId('pokemon-name');
+  expect(pokemonName).toBeInTheDocument();
+  expect(pokemonName).toHaveTextContent(/pikachu/i);
+
+  const weight = screen.getByTestId('pokemon-weight');
+  expect(weight).toBeInTheDocument();
+  expect(weight).toHaveTextContent(/average weight: 6.0 kg/i);
+
+  const detailsLink = screen.getByRole('link', { name: /More details/i });
+  expect(detailsLink).toBeInTheDocument();
+  expect(detailsLink).toHaveAttribute('href', '/pokemon/25');
+
+  await user.click(detailsLink);
+  const isFavorited = screen.getByText(/pokémon favoritado?/i);
+  await user.click(isFavorited);
+  const favoriteImage = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+  expect(favoriteImage).toBeInTheDocument();
+  expect(favoriteImage).toHaveAttribute('src', '/star-icon.svg');
+  expect(favoriteImage).toHaveAttribute('alt', 'Pikachu is marked as favorite');
+});
